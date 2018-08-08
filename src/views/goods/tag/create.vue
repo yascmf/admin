42b27aac4630e0676e01ece47f20c67ec964959a
raoyc <raoyc@foxmail.com>
2018-08-08 18:18:24 +0800

    完成创建标签

--- src/views/goods/tag/create.vue
+++ src/views/goods/tag/create.vue
@@ -1,20 +1,43 @@
 <template>
   <div class="app-container">
-    <div class="createPost-container">
-      <el-form class="form-container" ref="postForm">
-        <el-button v-loading="loading" style="margin-left: 10px;" type="success" @click="submitForm">新增
-        </el-button>
-        <div class="createPost-main-container">
-          <el-row>
-            <el-col :span="24">
-              <el-form-item style="margin-bottom: 40px;" prop="title">
-                <el-input type="text" class="article-textarea" :rows="1" autosize placeholder="请输入标签名称" v-model="postForm.content_short">
-                </el-input>
-              </el-form-item>
-            </el-col>
-          </el-row>
-        </div>
-      </el-form>
-    </div>
+    <el-form ref="form" :model="form" label-width="120px">
+      <el-form-item label="标签名称">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">创建</el-button>
+        <el-button @click="onCancel">取消</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        name: '',
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$message('submit!')
+    },
+    onCancel() {
+      this.$message({
+        message: 'cancel!',
+        type: 'warning'
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.line{
+  text-align: center;
+}
+</style>
+
