d97138d1d11135952d821d3385d1de397f5650ee
raoyc <raoyc@foxmail.com>
2018-11-07 20:08:25 +0800

    using component

---
+++ src/components/FastAdmin/index.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="app-container">
+
+    <div class="resource-operation">
+      <el-row>
+          <el-button icon='el-icon-plus' type="primary" @click.prevent.stop="moduleCreate">创建</el-button>
+      </el-row>
+    </div>
+
+    <el-table :data="list" v-loading.body="listLoading" border fit highlight-current-row style="width: 100%">
+
+      <el-table-column align="center" label="{{labels[scope.row.id]}}" width="80">
+        <template slot-scope="scope">
+          <span>{{scope.row.id}}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column width="100px" label="推荐位">
+        <template slot-scope="scope">
+          <span>{{scope.row.flag}}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column width="120px" align="center" label="标题">
+        <template slot-scope="scope">
+          <span>{{scope.row.title}}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column width="100px" label="slug">
+        <template slot-scope="scope">
+          <span>{{scope.row.slug}}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column width="100px" label="分类">
+        <template slot-scope="scope">
+          <span>{{scope.row.cid}}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column width="180px" align="center" label="创建时间">
+        <template slot-scope="scope">
+          <span>{{scope.row.created_at | parseTime('{y}-{m}-{d} {h}:{i}')}}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column align="center" label="操作" width="120">
+        <template slot-scope="scope">
+          <router-link :to="'/content/article/'+scope.row.id+'/edit'">
+            <el-button type="primary" size="small" icon="el-icon-edit">编辑</el-button>
+          </router-link>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <div class="pagination-container">
+      <el-pagination background @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="listQuery.page"
+        :page-sizes="[10,20,30,50]" :page-size="listQuery.limit" layout="total, sizes, prev, pager, next, jumper" :total="total">
+      </el-pagination>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import { fetchArticleList } from '@/api/content'
+
+export default {
+  name: 'fast-admin-index',
+  data () {
+    return {
+      module: '',
+      labels: [],
+      items: [],
+      total: 0,
+      listLoading: true,
+      listQuery: {
+        page: 1,
+        page_size: 15
+      }
+    }
+  },
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        published: 'success',
+        draft: 'info',
+        deleted: 'danger'
+      }
+      return statusMap[status]
+    }
+  },
+  created() {
+    this.getList()
+  },
+  methods: {
+    getList() {
+      this.listLoading = true
+      fetchArticleList(this.listQuery).then(response => {
+        console.log(response)
+        this.list = response.items
+        this.total = response.total
+        this.listLoading = false
+      })
+    },
+    handleSizeChange(val) {
+      this.listQuery.limit = val
+      this.getList()
+    },
+    handleCurrentChange(val) {
+      this.listQuery.page = val
+      this.getList()
+    },
+    tagNew() {
+      this.$router.push({ path: '/content/article/create' })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.edit-input {
+  padding-right: 100px;
+}
+.cancel-btn {
+  position: absolute;
+  right: 15px;
+  top: 10px;
+}
+</style>
