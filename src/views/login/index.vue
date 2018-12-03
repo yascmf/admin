c20d09f9f68c20c55ca41a0d562fd376282129a4
raoyc <raoyc@foxmail.com>
2018-12-03 16:11:48 +0800

    up login

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -21,10 +21,12 @@
           登录
         </el-button>
       </el-form-item>
+      <!--
       <div class="tips">
         <span style="margin-right:20px;">username: admin</span>
         <span> password: admin</span>
       </div>
+      -->
     </el-form>
   </div>
 </template>
